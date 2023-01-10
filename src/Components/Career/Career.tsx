adcfd75da8fa6ea6fc8b0dea2b53d130ecbd3411
AJ-Brown-InTech <ajalantbrown@gmail.com>
2023-01-10 12:53:36 -0600

    footer

--- src/Components/Career/Career.tsx
+++ src/Components/Career/Career.tsx
@@ -6,7 +6,7 @@ export default class Career extends Component {
       <section className='career'>
         <div className='c-cont'>
         <aside>
-        <span className='c-title'>The <span>Jist</span></span>
+        <span className='c-title'>The <span>jist</span></span>
         <p>I'm a military veteran and software developer.</p>
         <p>i inspire and get inspired through software. i've been building things since the golden age of tumblr.</p>
         <p>view my latest project "libre" (<a href='https://github.com/AJ-Brown-InTech?tab=repositories'>open source for now</a>)</p>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,18 @@ import '../../styles/footer.scss'
 export default class componentName extends Component {
   render() {
     return (
-  <div className='footer'>Footer</div>
+  <footer className='footer'>
+    <section className='f-cont'>
+        <article className='f-git' >get <span style={{color:'crimson'}}>in</span> touch</article>
+        <article className='f-links'>
+          <ul><a href='https://github.com/AJ-Brown-InTech'>Github</a></ul>
+          <ul><a href='https://www.linkedin.com/in/ajtbrown/'>linkedin</a></ul>
+          <ul>ajalantbrown@gmail.com</ul>
+          <ul>made in austin</ul>
+        </article>
+  </section>
+  
+  </footer>
     );
   }
 }
